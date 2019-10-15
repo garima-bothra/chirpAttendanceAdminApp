6fbc6b9ce9e265ec65c15aaeeba5497c070bbf68
garima94921 <gaarimabothra@gmail.com>
2019-10-15 14:30:41 +0530

    first commit

---
+++ chirpAttendanceAdmin/RoomViewController.swift
@@ -0,0 +1,86 @@
+//
+//  RoomViewController.swift
+//  chirpAttendanceAdmin
+//
+//  Created by Garima Bothra on 14/10/19.
+//  Copyright © 2019 Garima Bothra. All rights reserved.
+//
+
+import UIKit
+import Firebase
+import var CommonCrypto.CC_MD5_DIGEST_LENGTH
+import func CommonCrypto.CC_MD5
+import typealias CommonCrypto.CC_LONG
+
+
+class RoomViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func makeRoomButtonPressed(_ sender: Any) {
+    
+        var post1Ref = refer.child("Admin").child(adminName).child("rooms").childByAutoId()
+        
+        
+        var postId = post1Ref.key
+        print(postId)
+        let substr = postId?.prefix(5)
+    
+       
+        
+        func MD5(string: String) -> Data {
+            let length = Int(CC_MD5_DIGEST_LENGTH)
+            let messageData = string.data(using:.utf8)!
+            var digestData = Data(count: length)
+
+            _ = digestData.withUnsafeMutableBytes { digestBytes -> UInt8 in
+                messageData.withUnsafeBytes { messageBytes -> UInt8 in
+                    if let messageBytesBaseAddress = messageBytes.baseAddress, let digestBytesBlindMemory = digestBytes.bindMemory(to: UInt8.self).baseAddress {
+                        let messageLength = CC_LONG(messageData.count)
+                        CC_MD5(messageBytesBaseAddress, messageLength, digestBytesBlindMemory)
+                    }
+                    return 0
+                }
+            }
+            return digestData
+        }
+
+        
+        let md5Data = MD5(string: String(substr!))
+        let md5Hex =  md5Data.map { String(format: "%02hhx", $0) }.joined()
+        print("md5Hex: \(md5Hex)")
+       let hashed = md5Hex.prefix(5)
+//        let ref = Database.database().reference().child("Admin").child("rooms").child("").queryEqual(toValue: "569fe")
+//       print(ref)
+        
+        refer.child("Admin").child(adminName).child("rooms").observe(.value) { (snapshot) in
+            print("\((snapshot.value as? NSDictionary)!)")
+            
+        }
+//        ref.observeSingleEvent(of: .value, with: { snapshot in
+//            if(snapshot.exists()){
+//            print(snapshot)
+//            }
+//        })
+//        refer.child("Admin").child(adminName).child("rooms").observeSingleEvent(of: .value, with: { (snapshot) in
+//
+//        })
+        
+    }
+   
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- chirpAttendanceAdmin/ViewController.swift
+++ chirpAttendanceAdmin/ViewController.swift
@@ -7,14 +7,43 @@
 //
 
 import UIKit
+import Firebase
+
+
+var refer = Database.database().reference();
+var adminName : String = "";
+
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
-
-
+  
+   
+    @IBOutlet weak var incorrectUsernameNotification: UITextField!
+    @IBOutlet weak var passwordEntered: UITextField!
+    @IBOutlet weak var usernameEntered: UITextField!
+    
+     @IBAction func loginButtonPressed(_ sender: UIButton) {
+        
+       
+        refer.child("Admin").child(usernameEntered.text!).observeSingleEvent(of: .value, with: { (snapshot) in
+            // Get user value
+            let value = snapshot.value as? NSDictionary
+            let password = value?["authPassword"] as? String ?? ""
+              print(password);
+            if(password==self.passwordEntered.text!){
+                self.performSegue(withIdentifier: "goToRoom", sender: self)
+                adminName=self.usernameEntered.text!
+                print(adminName)
+            }
+            else
+            {
+                self.incorrectUsernameNotification.text = "Incorrect username/password";
+                self.usernameEntered.text = nil
+                self.passwordEntered.text = nil
+            }
+    })
+    
+    
+}
 }
 
