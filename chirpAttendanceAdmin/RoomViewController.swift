fabed4c40cfb109398bf49bfc5313ce0ad5ee390
garima94921 <gaarimabothra@gmail.com>
2019-10-15 19:50:45 +0530

    Basic layout done.Rooms are created in the firebase database using hashed key and unix time

--- chirpAttendanceAdmin/RoomViewController.swift
+++ chirpAttendanceAdmin/RoomViewController.swift
@@ -7,10 +7,7 @@
 //
 
 import UIKit
-import Firebase
-import var CommonCrypto.CC_MD5_DIGEST_LENGTH
-import func CommonCrypto.CC_MD5
-import typealias CommonCrypto.CC_LONG
+
 
 
 class RoomViewController: UIViewController {
@@ -23,56 +20,7 @@ class RoomViewController: UIViewController {
     
     @IBAction func makeRoomButtonPressed(_ sender: Any) {
     
-        var post1Ref = refer.child("Admin").child(adminName).child("rooms").childByAutoId()
-        
-        
-        var postId = post1Ref.key
-        print(postId)
-        let substr = postId?.prefix(5)
-    
-       
-        
-        func MD5(string: String) -> Data {
-            let length = Int(CC_MD5_DIGEST_LENGTH)
-            let messageData = string.data(using:.utf8)!
-            var digestData = Data(count: length)
-
-            _ = digestData.withUnsafeMutableBytes { digestBytes -> UInt8 in
-                messageData.withUnsafeBytes { messageBytes -> UInt8 in
-                    if let messageBytesBaseAddress = messageBytes.baseAddress, let digestBytesBlindMemory = digestBytes.bindMemory(to: UInt8.self).baseAddress {
-                        let messageLength = CC_LONG(messageData.count)
-                        CC_MD5(messageBytesBaseAddress, messageLength, digestBytesBlindMemory)
-                    }
-                    return 0
-                }
-            }
-            return digestData
-        }
-
-        
-        let md5Data = MD5(string: String(substr!))
-        let md5Hex =  md5Data.map { String(format: "%02hhx", $0) }.joined()
-        print("md5Hex: \(md5Hex)")
-       let hashed = md5Hex.prefix(5)
-//        let ref = Database.database().reference().child("Admin").child("rooms").child("").queryEqual(toValue: "569fe")
-//       print(ref)
-        
-        refer.child("Admin").child(adminName).child("rooms").observe(.value) { (snapshot) in
-            print("\((snapshot.value as? NSDictionary)!)")
-            
-        }
-//        ref.observeSingleEvent(of: .value, with: { snapshot in
-//            if(snapshot.exists()){
-//            print(snapshot)
-//            }
-//        })
-//        refer.child("Admin").child(adminName).child("rooms").observeSingleEvent(of: .value, with: { (snapshot) in
-//
-//        })
-        
-    }
-   
-    
+        self.performSegue(withIdentifier: "goToRoomForm", sender: (Any).self)
     /*
     // MARK: - Navigation
 
@@ -84,3 +32,5 @@ class RoomViewController: UIViewController {
     */
 
 }
+}
+
